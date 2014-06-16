42a9240ddfa59529faf1babf07278bde1e71a177
commel <commel@gmail.com>
2014-06-16 19:54:12 +0000

    weitere kleinere rspec-anpassungen

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Capybara::DSL  
+  
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -36,8 +38,6 @@ RSpec.configure do |config|
       example.run
     end
   end
-  
-  config.include Capybara::DSL  
   
   # load the seeds for the testdb as well
   config.before(:suite) do
