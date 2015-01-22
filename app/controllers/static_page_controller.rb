3bf75126ab4026f8def687dc5ee4ba17a2244668
Bernd Ritter <commel@gmail.com>
2015-01-22 12:01:34 +0100

    Footer ergänzt, Menü grob strukturiert. Breadcrumbs geben nun Seitennamen vor.

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,16 @@
 #encoding: utf-8
 class StaticPageController < ApplicationController
+
   def about
-    add_breadcrumb "Über", about_path
+    add_breadcrumb "Über Holarse", about_path
   end
+
+  def imprint
+    add_breadcrumb "Impressum", imprint_path
+  end
+
+  def privacy
+    add_breadcrumb "Datenschutzerklärung", privacy_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'about' => 'static_page#about', as: :about
+  get 'imprint' => 'static_page#imprint', as: :imprint
+  get 'privacy' => 'static_page#privacy', as: :privacy
 
   resources :users
   get 'register' => 'users#new', as: :register
