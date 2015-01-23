7fca34dd91c876b1b43961c7193e6ad2cd82f302
Bernd Ritter <commel@gmail.com>
2015-01-23 10:40:56 +0100

    Hübschere Fehlermeldung bei fehlerhaften Formular

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,8 +1,11 @@
+#encoding: utf-8
 module FormsHelper
 
-  def display_form_errors(errors)
-    content_tag :div do
-      errors.each do |error|
+  def display_form_errors(model)
+    return if model.errors.empty?
+    content_tag :div, class: "alert alert-danger fade in" do
+      concat content_tag :h4, "Im Formular sind fehlerhafte Einträge vorhanden"
+      model.errors.full_messages.each do |error|
         concat content_tag :p, error
       end
     end
