1c45cdd47be8de062ff74f31b33104fc985632bf
Bernd Ritter <commel@gmail.com>
2015-03-01 20:29:19 +0100

    Breadcrumbs für die Suche

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
 
+  add_breadcrumb "Suchergebnisse"
+
   def index
     @results = PerformSearch.(params[:q]).decorate
+    add_breadcrumb "#{@results.size} Treffer zu #{params[:q]}"
   end
 
 end
