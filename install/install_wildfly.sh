6e3a201242bed0e04b749c9d60c191e9fa5ae495
Bernd Ritter <comrad@holarse-linuxgaming.de>
2016-08-29 14:15:03 +0200

    Installationscript fertig, inkl. Bauen und Deployment von Holarse

--- install/install_wildfly.sh
+++ install/install_wildfly.sh
@@ -2,12 +2,13 @@
 
 WILDFLY="wildfly-10.0.0.Final"
 MYSQL_J="mysql-connector-java-5.1.39"
+MAVEN="apache-maven-3.3.9"
 
 ADMIN_USER="admin"
 ADMIN_PASS="geheim"
 
 rm -rf $WILDFLY
-if [ !-e "$WILDFLY.tar.gz" ]; then
+if [ ! -e "$WILDFLY.tar.gz" ]; then
 	wget http://download.jboss.org/wildfly/10.0.0.Final/$WILDFLY.tar.gz
 fi
 tar xfvz $WILDFLY.tar.gz
@@ -17,7 +18,7 @@ mkdir -p modules/system/layers/base/com/mysql/driver/main
 cd ..
 
 rm -rf $MYSQL_J
-if [ !-e "$MYSQL_J.tar.gz" ]; then
+if [ ! -e "$MYSQL_J.tar.gz" ]; then
 	wget http://dev.mysql.com/get/Downloads/Connector-J/$MYSQL_J.tar.gz
 fi
 
@@ -32,5 +33,15 @@ bin/jboss-cli.sh --connect --command="/subsystem=datasources/jdbc-driver=mysql:a
 bin/jboss-cli.sh --connect --command="data-source add --name=holarseDS --jndi-name=java:/holarseDS --driver-name=mysql --connection-url=jdbc:mysql://localhost:3306/holarse --user-name=holarse --password=geheim"
 
 bin/add-user.sh $ADMIN_USER $ADMIN_PASS
-
+cd ..
 echo "Fertig, Login nun möglich unter http://localhost:9990 mit $ADMIN_USER und $ADMIN_PASS"
+
+echo "Installation Holarse"
+rm -rf "$MAVEN"
+if [ ! -e "$MAVEN-bin.tar.gz" ]; then
+	wget "ftp://mirror.netcologne.de/apache.org/maven/maven-3/3.3.9/binaries/$MAVEN-bin.tar.gz"
+fi
+tar xfvz $MAVEN-bin.tar.gz
+cd ..
+install/$MAVEN/bin/mvn clean package
+install/$WILDFLY/bin/jboss-cli.sh --connect --command="deploy target/Holarse-1.0-SNAPSHOT.war"
