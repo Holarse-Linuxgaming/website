e7797a96cb9633e4fc0301f1d2dea225bfa85973
Kevin Rohland <nascher@holarse-linuxgaming.de>
2020-02-15 11:15:13 +0100

    Remove not null attribute from tag

--- src/main/java/de/holarse/backend/db/Tag.java
+++ src/main/java/de/holarse/backend/db/Tag.java
@@ -14,7 +14,7 @@ public class Tag extends Base {
 
     private Long weight;
     
-    @Column(name = "use_count", nullable = false, columnDefinition = "bigint default 0")
+    @Column(name = "use_count", columnDefinition = "bigint default 0")
     private Long useCount;
     
     @OneToOne
