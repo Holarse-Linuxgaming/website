e9b7af1d8196316920a4475bf69e8454b9b1dfc2
Kevin Rohland <nascher@holarse-linuxgaming.de>
2020-02-26 21:40:05 +0100

    Extend SiteMap for News and Articles

--- src/main/java/de/holarse/backend/db/repositories/NewsRepository.java
+++ src/main/java/de/holarse/backend/db/repositories/NewsRepository.java
@@ -1,7 +1,19 @@
 package de.holarse.backend.db.repositories;
 
 import de.holarse.backend.db.News;
+import de.holarse.web.seo.SiteMapNode;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface NewsRepository extends JpaRepository<News, Long>, SluggableBranchableRepository<News>, OldIdRepository<News>  {
+public interface NewsRepository
+        extends JpaRepository<News, Long>,
+                SluggableBranchableRepository<News>,
+                OldIdRepository<News> {
+
+    @Query(
+            value =
+                    "SELECT new de.holarse.web.seo.SiteMapNode(n.id, n.slug, n.created, n.updated, 'news')"
+                            + " FROM de.holarse.backend.db.News n")
+    List<SiteMapNode> findAllForSiteMap();
 }
