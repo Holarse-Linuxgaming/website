e816e052f20cbe27d55b1be45ac3404e775f08c6
Kevin Rohland <nascher@holarse-linuxgaming.de>
2020-03-07 13:02:30 +0100

    Some changes to SiteMap

--- src/main/java/de/holarse/web/sitemap/W3cDateFormatter.java
+++ src/main/java/de/holarse/web/sitemap/W3cDateFormatter.java
@@ -5,31 +5,35 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class W3cDateFormatter extends StdSerializer<OffsetDateTime> {
+    static DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm'Z'");
 
     public W3cDateFormatter() {
         this(null);
     }
-    
+
     public W3cDateFormatter(Class<OffsetDateTime> t) {
         super(t);
     }
 
-    
     /**
      * siehe Datumsformat: https://www.w3.org/TR/NOTE-datetime
-     * 
-     * Complete date plus hours and minutes: YYYY-MM-DDThh:mmTZD (eg 1997-07-16T19:20+01:00) 
+     *
+     * <p>Complete date plus hours and minutes: YYYY-MM-DDThh:mmTZD (eg 1997-07-16T19:20+01:00)
+     *
      * @param date
      * @param gen
      * @param arg2
      * @throws java.io.IOException
      */
     @Override
-    public void serialize(final OffsetDateTime date, final JsonGenerator gen, final SerializerProvider arg2) throws IOException {
-        gen.writeString(date.format(DateTimeFormatter.ISO_DATE_TIME));
+    public void serialize(
+            final OffsetDateTime date, final JsonGenerator gen, final SerializerProvider arg2)
+            throws IOException {
+        gen.writeString(
+                date.toInstant().atZone(ZoneId.of("UTC")).format(W3cDateFormatter.dateTimeFormat));
     }
-    
 }
