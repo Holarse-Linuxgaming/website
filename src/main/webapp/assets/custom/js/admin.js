665ae7e57ce3f1daa3e438436dbad9ae6c73f81c
Bernd Ritter <comrad@holarse-linuxgaming.de>
2020-04-10 23:00:36 +0200

    Nachtrag #164

--- src/main/webapp/assets/custom/js/admin.js
+++ src/main/webapp/assets/custom/js/admin.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
    
    $(".datepicker").datepicker($.datepicker.regional[ "de" ]);
    
-   var v_admin_drueckblick = new Vue({
+   var vadmdbl = new Vue({
             el: "#v-admin-drueckblick",
             data: {
                entries: [],
-               msg: "Hello world"
             },
             methods: {
                load_data: function() {
@@ -14,14 +13,13 @@ $(document).ready(function() {
                      dataType: "json",
                      url: "/admin/drueckblick",
                      success: function(result) {
-                        entries = result;
-                        console.debug("Loaded " + entries.length + " entries...");
+                        vadmdbl.entries = result;
                      }
                   });
                }
             },
             mounted: function() {
-               //this.load_data();
+               this.load_data();
             }
    });
 
