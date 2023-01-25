2ad822b0fe601a00bf91e1b9822748a41f4378f2
Bernd Ritter <comrad@posteo.de>
2023-01-25 16:53:46 +0100

    Thymeleaf Links

--- src/main/java/de/holarse/web/controller/RegisterController.java
+++ src/main/java/de/holarse/web/controller/RegisterController.java
@@ -15,7 +15,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
