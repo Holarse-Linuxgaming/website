35a3c7eccfecfa9176ad13359aa3163662a24c71
Bernd Ritter <comrad@holarse-linuxgaming.de>
2024-02-22 22:48:53 +0100

    Search MV is now updated after edit

--- src/main/java/de/holarse/queues/consumers/SearchWorker.java
+++ src/main/java/de/holarse/queues/consumers/SearchWorker.java
@@ -3,12 +3,18 @@ package de.holarse.queues.consumers;
 import de.holarse.backend.db.repositories.SearchRepository;
 import de.holarse.queues.commands.SearchRefresh;
 import static de.holarse.config.JmsQueueTypes.QUEUE_SEARCH;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * Der SearchWorker nimmt Anfragen entgegen, mit denen die Suche angepasst oder
+ * aktualisiert werden kann.
+ * @author comrad
+ */
 @Component
 public class SearchWorker {
     
@@ -17,6 +23,7 @@ public class SearchWorker {
     @Autowired
     private SearchRepository searchRepository;
     
+    @Transactional
     @JmsListener(destination = QUEUE_SEARCH)
     public void importDrückblickEntries(final SearchRefresh cmd) {
         log.debug("Incoming Command to Refresh Materialized View");
